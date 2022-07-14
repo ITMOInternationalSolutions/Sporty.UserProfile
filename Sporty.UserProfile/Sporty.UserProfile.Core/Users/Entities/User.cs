f45bf2e7c043bd12fdcefad00b34e660054ad078
nikolaykochubeev <nikolay.kochubeev@gmail.com>
2022-07-15 01:11:14 +0300

    feat: added UserType

--- Sporty.UserProfile/Sporty.UserProfile.Core/Users/Entities/User.cs
+++ Sporty.UserProfile/Sporty.UserProfile.Core/Users/Entities/User.cs
@@ -1,8 +1,9 @@
-﻿namespace Sporty.UserProfile.Core.Users;
+﻿namespace Sporty.UserProfile.Core.Users.Entities;
 
 public class User
 {
     public Guid Id { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
+    public UserType UserType { get; set; }
 }
